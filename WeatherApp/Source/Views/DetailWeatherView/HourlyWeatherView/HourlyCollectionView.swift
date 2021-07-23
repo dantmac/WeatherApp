69c8c33b397358d426578a61f69cc153210c8f2c
dantmac <dantmac@mail.ru>
2021-07-23 20:31:31 +0300

    interface building. stage 1.1

---
+++ WeatherApp/Source/Views/DetailWeatherView/HourlyWeatherView/HourlyCollectionView.swift
@@ -0,0 +1,8 @@
+//
+//  HourlyCollectionView.swift
+//  WeatherApp
+//
+//  Created by admin on 23.07.2021.
+//
+
+import Foundation
---
+++ WeatherApp/Source/Views/DetailWeatherView/HourlyWeatherView/HourlyWeatherViewCell/HourlyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  HourlyCollectionViewCell.swift
+//  WeatherApp
+//
+//  Created by admin on 23.07.2021.
+//
+
+import Foundation
+import UIKit
+
+class HourlyCollectionViewCell: UICollectionViewCell {
+    
+}
